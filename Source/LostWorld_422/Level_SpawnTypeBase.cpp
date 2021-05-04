5fe709a5d2bcaded7e2e678153e9bd1e357fea9f
Zekraviax <zekraviax@gmail.com>
2021-05-05 08:48:37 +1000

    Progress on refactoring level generation

--- Source/LostWorld_422/Level_SpawnTypeBase.cpp
+++ Source/LostWorld_422/Level_SpawnTypeBase.cpp
@@ -1,17 +1,38 @@
 #include "Level_SpawnTypeBase.h"
 
+#include "BaseClass_GridTile.h"
 
-Level_SpawnTypeBase::Level_SpawnTypeBase()
+
+// Sets default values
+ALevel_SpawnTypeBase::ALevel_SpawnTypeBase()
 {
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Constructors
+	//static ConstructorHelpers::FObjectFinder<ABaseClass_GridTile> GridTile_BlueprintConstruct(TEXT("Blueprint'/Game/Level_GridTile_Blueprint.Level_GridTile_Blueprint'"));
+	//if (GridTile_BlueprintConstruct.Object) {
+	//	GridTile_Class = (UClass*)GridTile_BlueprintConstruct.Object->StaticClass;
+	//}
 }
 
-Level_SpawnTypeBase::~Level_SpawnTypeBase()
+// Called when the game starts or when spawned
+void ALevel_SpawnTypeBase::BeginPlay()
 {
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALevel_SpawnTypeBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
 }
 
 
 // ------------------------- Base Class Functions
-void Level_SpawnTypeBase::RunLevelGenerationFunction()
+void ALevel_SpawnTypeBase::RunLevelGeneratorFunction()
 {
 
 }

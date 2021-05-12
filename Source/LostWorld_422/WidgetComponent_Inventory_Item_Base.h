0cf2344f7dcac6f1c0679dd9cb024a4da8a31b17
Zekraviax <zekraviax@gmail.com>
2021-05-12 21:44:53 +1000

    Begin work on Item-related widgets

---
+++ Source/LostWorld_422/WidgetComponent_Inventory_Item_Base.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WidgetComponent_Inventory_Item_Base.generated.h"
+
+
+UCLASS()
+class LOSTWORLD_422_API UWidgetComponent_Inventory_Item_Base : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};

86c30a0dc5cf9b9156d8ec21e6d34555016caf81
Zekraviax <zekraviax@gmail.com>
2021-05-15 18:53:13 +1000

    Fixed some bugs and oversights with the Inventory  /  Sol Ring is now the first fully working Equippable Item

--- Source/LostWorld_422/WidgetComponent_Inventory_Item_Base.cpp
+++ Source/LostWorld_422/WidgetComponent_Inventory_Item_Base.cpp
@@ -7,6 +7,8 @@
 // ------------------------- Item
 void UWidgetComponent_Inventory_Item_Base::OnEquipButtonPressed()
 {
+	FActorSpawnParameters SpawnParameters;
+
 	// Equip item
 	if (ItemEquipTextWidget->GetText().ToString() == "Equip") {
 		// Find a slot that the item can be equipped to
@@ -19,17 +21,32 @@ void UWidgetComponent_Inventory_Item_Base::OnEquipButtonPressed()
 					Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->PlayerInventory.Add(FoundWidget->ItemData);
 
 				FoundWidget->ItemData = ItemData;
-
 				Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->PlayerInventory.Remove(ItemData);
+				Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->EntityInBattleRef->EquippedItems.Add(ItemData);
+
+				// Run item equip function
+				AItemFunctions_BaseClass* ItemAbilityActor_Reference = GetWorld()->SpawnActor<AItemFunctions_BaseClass>(ItemData.Functions, FVector::ZeroVector, FRotator::ZeroRotator, SpawnParameters);
+				ItemAbilityActor_Reference->TriggeredFunction_OnItemEquipped(Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->EntityInBattleRef);
+				ItemAbilityActor_Reference->ConditionalBeginDestroy();
+
+				break;
 			}
 		}
 	} else if (ItemEquipTextWidget->GetText().ToString() == "Unequip") {
 		// Unequip item
-		//if (FoundWidget->ItemData.DisplayName != "Default")
-			//Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->PlayerInventory.Add(FoundWidget->ItemData);
-
 		Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->PlayerInventory.Add(ItemData);
+		Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->EntityInBattleRef->EquippedItems.Remove(ItemData);
+
+		// Run item unequip function
+		AItemFunctions_BaseClass* ItemAbilityActor_Reference = GetWorld()->SpawnActor<AItemFunctions_BaseClass>(ItemData.Functions, FVector::ZeroVector, FRotator::ZeroRotator, SpawnParameters);
+
+		if (ItemAbilityActor_Reference->IsValidLowLevel()) {
+			ItemAbilityActor_Reference->TriggeredFunction_OnItemUnequipped(Cast<ABaseClass_PlayerController>(GetWorld()->GetFirstPlayerController())->EntityInBattleRef);
+			ItemAbilityActor_Reference->ConditionalBeginDestroy();
+		}
+
 		ItemData = F_Item_Base();
+		UpdateWidget();
 	}
 
 	for (TObjectIterator<UWidget_Inventory_Base> Itr; Itr; ++Itr) {
--- Source/LostWorld_422/Widget_Inventory_Base.cpp
+++ Source/LostWorld_422/Widget_Inventory_Base.cpp
@@ -16,24 +16,27 @@ void UWidget_Inventory_Base::OnInventoryOpened(ABaseClass_PlayerController* Play
 	}
 
 	// Get all equipped items and add them to the EquippedItemsScrollBox
-	for (int i = 0; i < PlayerControllerReference->EntityInBattleRef->EquippedItems.Num(); i++) {
-		for (int x = 0; x < EquippedItemsScrollBox->GetChildrenCount(); x++) {
-			if (Cast<UWidgetComponent_Inventory_Item_Base>(EquippedItemsScrollBox->GetChildAt(x))) {
-				InventoryItem_Reference = Cast<UWidgetComponent_Inventory_Item_Base>(EquippedItemsScrollBox->GetChildAt(x));
+	for (int x = 0; x < EquippedItemsScrollBox->GetChildrenCount(); x++) {
+		if (Cast<UWidgetComponent_Inventory_Item_Base>(EquippedItemsScrollBox->GetChildAt(x))) {
+			InventoryItem_Reference = Cast<UWidgetComponent_Inventory_Item_Base>(EquippedItemsScrollBox->GetChildAt(x));
+			InventoryItem_Reference->ItemData = F_Item_Base(); // Fixes that annoying auto-equipping Sol Ring
 
+			for (int i = 0; i < PlayerControllerReference->EntityInBattleRef->EquippedItems.Num(); i++) {
 				if (InventoryItem_Reference->IsValidLowLevel()) {
-					if (InventoryItem_Reference->StaticEquipmentSlot &&
-						PlayerControllerReference->EntityInBattleRef->EquippedItems[i].EquipSlots.Contains(InventoryItem_Reference->StaticEquipmentSlotType)) {
-						InventoryItem_Reference->ItemData = PlayerControllerReference->EntityInBattleRef->EquippedItems[i];
+					if (InventoryItem_Reference->StaticEquipmentSlot) { 
+						if (PlayerControllerReference->EntityInBattleRef->EquippedItems[i].EquipSlots.Contains(InventoryItem_Reference->StaticEquipmentSlotType)) {
+							InventoryItem_Reference->ItemData = PlayerControllerReference->EntityInBattleRef->EquippedItems[i];
+							break;
+						}
 					}
 				}
-
-				InventoryItem_Reference->UpdateWidget();
 			}
+
+			InventoryItem_Reference->UpdateWidget();
 		}
 	}
 
-	// Get all unequipped items and add them to the UnequippedItemsScrollBox
+	//Get all unequipped items and add them to the UnequippedItemsScrollBox
 	if (InventoryItem_Class)
 		UnequippedItemsScrollBox->ClearChildren();
 
